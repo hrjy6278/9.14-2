fcda0d0c0c0cc3c5267385cf2d44d58a6a1a70e4
hrjy6278 <hrjy6278@naver.com>
2020-09-14 14:46:10 +0900

    new

--- 9.14-2/ViewController.swift
+++ 9.14-2/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    
+}
