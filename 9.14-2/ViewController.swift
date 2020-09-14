5cab0ca2d76679b013e39d52ef4d6db50770c35b
hrjy6278 <hrjy6278@naver.com>
2020-09-14 14:41:01 +0900

    Initial Commit

---
+++ 9.14-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  9.14-2
+//
+//  Created by kimjaeyoun on 2020/09/14.
+//  Copyright © 2020 kimjaeyoun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
